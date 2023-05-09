d24453dbcb74b9635ecfda3a090caae5e42742d3
Nils Matic <matic1909@gmail.com>
2023-05-09 14:15:56 +0200

    some styling

---
+++ src/components/Home.js
@@ -0,0 +1,5 @@
+function Home() {
+  const homeContainer = document.createElement("main");
+  const homeText = document.createElement("p");
+  homeText.textContent = "Experience authentic Japanese food in Lippstadt";
+}
