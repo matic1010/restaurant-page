bc3f9296f4557afb47b0f1bc1dc6512b7475dc8d
Nils Matic <matic1909@gmail.com>
2023-05-09 14:51:16 +0200

    added main home content

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,17 @@
-function Home() {
+export default function Home() {
   const homeContainer = document.createElement("main");
+  const homeInfo = document.createElement("div")
+  homeInfo.classList.add("info");
   const homeText = document.createElement("p");
-  homeText.textContent = "Experience authentic Japanese food in Lippstadt";
+  homeText.textContent = "Enoy the freshest coffe, roasted in house and prepared by our expert baristas";
+  const imageContainer = document.createElement("div");
+  imageContainer.classList.add("home-image");
+  homeInfo.appendChild(homeText);
+  homeInfo.appendChild(imageContainer);
+  const bottomText = document.createElement("p");
+  bottomText.textContent = "Pay us a visit or order our freshly roasted beans here!";
+  homeInfo.appendChild(bottomText)
+  homeContainer.appendChild(homeInfo);
+
+  return homeContainer;
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import Header from "./components/Header";
+import Home from "./components/Home";
 import "./style.css";
 
 const contentContainer = document.getElementById("content");
 
 contentContainer.appendChild(Header())
+contentContainer.append(Home())
